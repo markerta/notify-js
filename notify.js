c3ffdbebb4571c71e6820c3239de6595c25c2d5c
Clayton M Billups <magnus.billups@ibm.com>
2017-03-19 21:26:42 -0400

    updated function call

--- notify.js
+++ notify.js
@@ -3,8 +3,11 @@ var prevNum = 0;
 var curNum = 0;
 var myRegex = RegExp('[0-9]?[0-9]?[0-9]');
 var counter = 0;
+
+console.log("Setuo complete");
 var notify = new function (){
-  while(counter < 250){
+  while(counter < 2){
+    console.log('open site');
     page.open("https://www.rev.com/workspace/findwork/transcription", function(){
         if (status !== 'success') {
           console.log('Unable to access network');
@@ -14,10 +17,17 @@ var notify = new function (){
               if(curNum > prevNum)
               //output sound
               prevNum = curNum;
-              counter += 1;
-              phantom.exit();
+              
+              
             });
         }
     });
+
+    counter += 1;
   }
+
+  console.log("DONE!");
+  phantom.exit();
 }
+
+notify();
